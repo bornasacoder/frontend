e1c2e7f09f14c2b7dd63a808823bf513f212b022
Arjun Singh <117923579+aarjun8060@users.noreply.github.com>
2023-11-21 15:37:06 +0530

    first

--- src/directives/login.js
+++ src/directives/login.js
@@ -1,14 +1,47 @@
 import React from 'react'
 import Header from './header'
 import {Link} from "react-router-dom"
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import toastr from 'reactjs-toastr';
+import axios from 'axios';
 
 function Login() {
+    const initialValues = { 
+        email: '',
+        password: '',
+    }
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().email('Email is required').required(),
+        password: Yup.string().required('Password is required'),
+      });
+
+      const headers = {
+        'Content-Type': 'text/plain'
+     };
+
+    const onSubmit = async (values) => {
+        console.log(values)
+        try{
+            const data = values
+            const response = await axios.post(`${process.env.REACT_APP_URL}/api/users/login`, data,{headers})
+            console.log(response)
+        } catch(err){
+            console.log(err)
+        }
+      }
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const formik = useFormik({
+          initialValues,
+          onSubmit ,
+          validationSchema
+      })
   return (
     <div style={{backgroundColor:"#323132",height:"100%",overflow:"hidden"}}>
         <div>
             <Header/>
         </div>
-        <div style={{display:"flex",justifyContent:"center",alignItems:"center",width:"100%",marginTop:"50px",height:"100vh"}}>
+        <div style={{display:"flex",justifyContent:"center",alignItems:"center",width:"100%",marginTop:"50px",height:"100%"}}>
             <div className='loginContentDiv' style={{width:"90%",display:"flex",marginTop:"100px",height:"100%"}}>
                 <div className='loginContentDivVideo' >
                         <video autoPlay loop muted className='loginContentDivVideo2' style={{width:"100%"}}>
@@ -24,25 +57,27 @@ function Login() {
                                                             <h3 className="purple oR m0">Join collective Transformation</h3>
                                                         </div>
                                                         <form 
-                                                        // onSubmit={this.submitForm}
+                                                        onSubmit={formik.handleSubmit}
                                                         >
                                                             <div className="body">
                                                                 <div className="input-box">
                                                                     <input placeholder="Email" 
-                                                                    // value={this.state.email}
-                                                                    //     onChange={this.onChange}
-                                                                         name="email" className="input11" type="email" />
+                                                                    value={formik.values.email}
+                                                                        onChange={formik.handleChange}
+                                                                        onBlur={formik.handleBlur}
+                                                                        name="email" className="input11" type="email" />
                                                                     <span style={{ position: 'absolute' }}><i className="fa fa-user"></i></span>
                                                                 </div>
-                                                                {/* <span className="errorMessage_signup">{this.state.emailError}</span> */}
+                                                                <span className="errorMessage_signup">{formik.errors.email}</span>
                                                                 <div className="input-box">
                                                                     <input placeholder="Password" type="password"
-                                                                    //  value={this.state.password}
-                                                                    //     onChange={this.onChange}
+                                                                     value={formik.values.password}
+                                                                        onBlur={formik.handleBlur}
+                                                                        onChange={formik.handleChange}
                                                                          name="password" className="input11" />
                                                                     <span style={{ position: 'absolute' }}><i className="fa fa-key"></i></span>
                                                                 </div>
-                                                                {/* <span className="errorMessage_signup">{this.state.passwordError}</span> */}
+                                                                <span className="errorMessage_signup">{formik.errors.password}</span>
 
                                                             </div>
                                                             <div className="foot">
--- src/directives/register.js
+++ src/directives/register.js
@@ -1,7 +1,47 @@
 import React from 'react'
 import Header from './header'
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import toastr from 'reactjs-toastr';
+import axios from 'axios';
 
 function Register() {
+    const initialValues = {
+        full_name: '',
+        email: '',
+        password: '',
+        confirm_password: '',
+        referral_code: '',
+    }
+    const validationSchema = Yup.object().shape({
+        full_name: Yup.string().required('First Name is required'),
+        email: Yup.string().email('Email is required').required(),
+        password: Yup.string().required('Password is required'),
+        confirm_password: Yup.string().required('Confirm Password is required').oneOf([Yup.ref('password'), null], 'Password and Confirm Password Should be same')
+      });
+
+      const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+     };
+
+    const onSubmit = async (values) => {
+        console.log(values)
+        try{
+            const data = values
+            const response =await axios.post(`${process.env.REACT_APP_URL}/api/users/register`, data,{headers} )
+            console.log(response)
+        } catch(err){
+            console.log(err)
+        }
+      }
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const formik = useFormik({
+          initialValues,
+          onSubmit ,
+          validationSchema
+      })
   return (
     <div style={{backgroundColor:"#323132",height:"100%",overflow:"hidden"}}>
         <div>
@@ -26,59 +66,63 @@ function Register() {
                                 <h3 className="modal-title1  blue oR m0">REGISTER</h3>
                                 <span className="light oR register_popup" style={{ fontSize: '14px' }} >Enter your informations to register.</span>
                             </div>
-                            <form 
-                            // onSubmit={this.submitForm}
+                            <form onSubmit={formik.handleSubmit}
                             >
                                 <div className="modal-body">
                                     <div className="input-box">
                                         <input placeholder="Full Name"
-                                        //  value={this.state.full_name} onChange={this.onChange} 
+                                            onBlur={formik.handleBlur}
+                                         value={formik.values.full_name} onChange={formik.handleChange} 
                                          name="full_name" type="text" />
                                         <span className="contact_footer"><i className="fa fa-user"></i></span>
                                     </div>
                                     <div className="errorMessage_signup">
-                                    {/* {this.state.firstnameError} */}
+                                    {formik.errors.full_name}
                                     </div>
                                     <div className="input-box">
                                         <input placeholder="Email Address" 
-                                        // value={this.state.email} onChange={this.onChange} 
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.email} onChange={formik.handleChange} 
                                         name="email" type="email" />
                                         <span className="contact_footer"><i className="fa fa-envelope-o"></i></span>
                                     </div>
                                     <div className="errorMessage_signup">
-                                    {/* {this.state.emailError} */}
+                                    {formik.errors.email}
                                     </div>
                                     <div className="input-box">
                                         <input placeholder="Login Key" type="password" 
-                                        // value={this.state.password} onChange={this.onChange}
+                                        onBlur={formik.handleBlur}
+                                        value={formik.values.password} onChange={formik.handleChange}
                                          name="password" />
                                         <span className="contact_footer"><i className="fa fa-key"></i></span>
                                     </div>
                                     <div className="errorMessage_signup">
-                                        {/* {this.state.passwordError} */}
+                                        {formik.errors.password}
                                         </div>
                                     <div className="input-box">
                                         <input placeholder="Confirm Login Key" type="password" 
-                                            // value={this.state.confirm_password} onChange={this.onChange} 
+                                            onBlur={formik.handleBlur}
+                                            value={formik.values.confirm_password} onChange={formik.handleChange} 
                                             name="confirm_password" />
                                         <span className="contact_footer"><i className="fa fa-key"></i></span>
                                     </div>
                                     <div className="errorMessage_signup">
-                                    {/* {this.state.confirmpasswordError} */}
+                                    {formik.errors.confirm_password}
                                     </div>
                                     <div className="errorMessage_confp">
-                                    {/* {this.state.confirmpassword1Error} */}
+                                    {/* {formik.errors.confirm_password} */}
                                     </div>
                                     <div className="input-box">
                                         <input placeholder="Referral Code" 
-                                        // value={this.state.referral_code}
-                                        //     onChange={this.onChange} 
+                                        value={formik.values.referral_code}
+                                            onChange={formik.handleChange} 
+                                            onBlur={formik.handleBlur}
                                             name="referral_code" type="text" />
                                         <span className="contact_footer"><i className="fa fa-retweet"></i></span>
                                     </div>
                                 </div>
                                 <div className="modal-footer">
-                                    <button type="submit" className="btn btn-gradient">Register</button>
+                                    <button type="submit" className="btn btn-gradient" >Register</button>
                                 </div>
                             </form>
                         </div>
